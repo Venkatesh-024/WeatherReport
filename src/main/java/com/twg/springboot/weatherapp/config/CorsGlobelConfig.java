2cd7facd091bd5cc3a9f438a00191bdf71cd54ed
Venkatesh-024 <kuppalavenkatesh9949@gmail.com>
2025-06-21 18:22:52 +0530

    weather

--- src/main/java/com/twg/springboot/weatherapp/config/CorsGlobelConfig.java
+++ src/main/java/com/twg/springboot/weatherapp/config/CorsGlobelConfig.java
@@ -3,6 +3,8 @@ package com.twg.springboot.weatherapp.config;
 
 
 
+
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
@@ -18,7 +20,9 @@ public class CorsGlobelConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins(
-                            "https://6856a092579051d979a29f1b--lucky-swan-55d5c7.netlify.app"
+                            "https://6856a092579051d979a29f1b--lucky-swan-55d5c7.netlify.app",
+                            "http://localhost:5173",
+                            "http://192.168.1.14:5173"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
